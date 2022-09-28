f7216e6e6b370fe1c79e1729013066d3c2ad3766
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2022-09-28 14:16:10 +0300

    Removing excess usings.

--- MainLibrary/Writer/FileWriter.cs
+++ MainLibrary/Writer/FileWriter.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MainLibrary.Writer
+﻿namespace MainLibrary.Writer
 {
     public class FileWriter: IWriter
     {
-        private string _filePath;
+        private readonly string _filePath;
 
         public FileWriter(string filePath)
         {
