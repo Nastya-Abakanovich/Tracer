648f7cd46869f34d97116b04427d1344494e85b9
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2022-09-30 15:39:14 +0300

    Made small changes.

--- MainLibrary/Tracer.cs
+++ MainLibrary/Tracer.cs
@@ -1,19 +1,17 @@
 ﻿using System.Diagnostics;
 using MainLibrary.ChangeableResult;
 using MainLibrary.Result;
-//using MainLibrary.Serialization;
 
 namespace MainLibrary
 {
     public class Tracer : ITracer
     {
-        private TraceResult _result;
-        private ChangeableTraceResult? _traceResult;
+        private ChangeableTraceResult _changeableTraceResult;
         private Stack<StackMethodInfo> _methodOrder;
 
         public Tracer()
         {
-            _traceResult = new ChangeableTraceResult();
+            _changeableTraceResult = new ChangeableTraceResult();
             _methodOrder = new Stack<StackMethodInfo>();
         }
 
@@ -21,11 +19,11 @@ namespace MainLibrary
         {
             ChangeableThreadInfo currThreadInfo;
             int currId = Thread.CurrentThread.ManagedThreadId;
-            currThreadInfo = _traceResult.Threads.Find(x => (x.Id == currId));
+            currThreadInfo = _changeableTraceResult.Threads.Find(x => (x.Id == currId));
             if (currThreadInfo == null)
             {
-                _traceResult?.Threads.Add(new ChangeableThreadInfo(currId));
-                currThreadInfo = _traceResult?.Threads[_traceResult.Threads.Count - 1];
+                _changeableTraceResult?.Threads.Add(new ChangeableThreadInfo(currId));
+                currThreadInfo = _changeableTraceResult?.Threads[_changeableTraceResult.Threads.Count - 1];
             }
             return currThreadInfo;
         }
@@ -56,10 +54,11 @@ namespace MainLibrary
             StackMethodInfo currStackMethodInfo = _methodOrder.Pop();
 
             currStackMethodInfo.MethodStopwatch?.Stop();
-            currStackMethodInfo.Method.LeadTime = currStackMethodInfo.MethodStopwatch.Elapsed.TotalSeconds;
+            currStackMethodInfo.Method.LeadTime = currStackMethodInfo.MethodStopwatch.Elapsed.TotalMilliseconds;
 
-            if (_methodOrder.TryPop(out parentStackMethodInfo))
+            if (_methodOrder.Count != 0)
             {
+                parentStackMethodInfo = _methodOrder.Pop();
                 parentStackMethodInfo.Method.Methods.Add(currStackMethodInfo.Method);
                 _methodOrder.Push(parentStackMethodInfo);
             }
@@ -72,10 +71,7 @@ namespace MainLibrary
 
         public TraceResult GetTraceResult()
         {
-         //   XmlSerialization ser = new XmlSerialization();
-         //   string s = ser.Serialize(_traceResult);
-            _result = new TraceResult(_traceResult);
-            return _result;
+            return new TraceResult(_changeableTraceResult);
         }
     }
 }
